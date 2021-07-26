2dcb80f69259df5b7feaf2f152b1f5d81bd498f1
TerenceLau <t_lau@uicstat.com>
2021-07-26 10:49:23 +0800

    Update and rename user_listen.md to user_listen.sh

---
+++ user_listen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 0. root (sudo su)
+# 1. create the `log` file
+# 2. change to file's owner and permission
+
+
+mkdir -p /var/log/usermonitor/
+echo usermonitor >/var/log/usermonitor/usermonitor.log
+chown nobody:nogroup /var/log/usermonitor/usermonitor.log
+chmod 002 /var/log/usermonitor/usermonitor.log
+chattr +a /var/log/usermonitor/usermonitor.log
+
+# add this two scripts to `/etc/profile`:
+
+echo "export HISTORY_FILE=/var/log/usermonitor/usermonitor.log" >> /etc/profile
+echo "export PROMPT_COMMAND='{ date "+%y-%m-%d %T ##### $(who am i |awk "{print \$1\" \"\$2\" \"\$5}") #### $(whoami) #### $(history 1 | { read x cmd; echo "$cmd"; })"; }>>$HISTORY_FILE" >> /etc/profile
