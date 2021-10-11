48b3dd2ec2e73b31f583e4ef7a93e249ff65c314
TerenceLau <t_lau@uicstat.com>
2021-10-11 11:03:49 +0800

    Update slurm-api.py

--- slurm-api.py
+++ slurm-api.py
@@ -4,7 +4,7 @@ from flask import Flask, abort, request, jsonify
 app = Flask(__name__)
 
 ## sinfo
-@app.route('/v1/sinfo/', methods=['GET'])
+@app.route('/api/v1.0/sinfo/', methods=['GET'])
 def py_sinfo():
     sinfo = subprocess.run(["sinfo", "-l"], stdout = subprocess.PIPE) ## binary string
     # print(sinfo.stdout.decode("ascii"))
@@ -19,11 +19,12 @@ def py_sinfo():
     pd_sinfo = pd.DataFrame(pd_sinfo)
     header = pd_sinfo.iloc[0]
     sinfo = pd.DataFrame(pd_sinfo.values[1:], columns = header)
-    return jsonify(sinfo.to_json())
+    return sinfo.to_json()
 
 ## sjob (based on `sacct`)
-@app.route('/v1/sjob/', methods=['GET'])
+@app.route('/api/v1.0/sjob', methods=['GET'])
 def py_job(username:str):
+  username = request.args.get("username")
   sjob = subprocess.run(["sacct", "-u", "{}".format(username)], stdout = subprocess.PIPE) # getpass.getuser() -> get username from bash (import getpass)
   sjob = sjob.stdout.decode("ascii").split("\n")
   out_length = (sjob[:-1])
@@ -35,7 +36,7 @@ def py_job(username:str):
   header = pd_sjob.iloc[0]
   pd_sjob = pd.DataFrame(pd_sjob.values[1:], columns = header)
     
-  return jsonify(sinfo.to_json())
+  return pd_sjob.to_json()
   
 if __name__ == "__main__":
   '''
@@ -45,3 +46,10 @@ if __name__ == "__main__":
   print("your tasks/jobs:\n", sjob)
   '''
   app.run(host="0.0.0.0", port=8080, debug=True)
+
+'''
+HOWTO
+1. sinfo: http://<IP_ADDR>/api/v1.0/sinfo/
+2. sjob: http://<IP_ADDR>/api/v1.0/sjob?username=<USERNAME>
+'''
+
