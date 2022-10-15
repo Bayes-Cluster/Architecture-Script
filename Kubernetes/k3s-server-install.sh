906f4c994cba8997bbd664cd3a7b32c8aefa33c1
TerenceLau <t_lau@uicstat.com>
2022-10-15 13:06:02 +0800

    Update k3s-server-install.sh

--- Kubernetes/k3s-server-install.sh
+++ Kubernetes/k3s-server-install.sh
@@ -1,5 +1,5 @@
 curl -sfL https://rancher-mirror.oss-cn-beijing.aliyuncs.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn  INSTALL_K3S_EXEC=" \
-                        --datastore-endpoint=mysql://db-user:db-passwd@#@tcp(db-ip:3306)/k3s \
+                        --datastore-endpoint=mysql://db-user:db-passwd@tcp(db-ip:3306)/k3s \
 						--token=$(openssl -hex 16) \
                         --node-ip $(ip addr show wg0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1) \
                         --advertise-address $(ip addr show wg0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1) \
