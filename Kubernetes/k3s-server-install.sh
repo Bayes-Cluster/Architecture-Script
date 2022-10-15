fbb42055c173cbfb9bd661d16b0f1581657963aa
TerenceLau <t_lau@uicstat.com>
2022-10-15 13:08:19 +0800

    Update k3s-server-install.sh

--- Kubernetes/k3s-server-install.sh
+++ Kubernetes/k3s-server-install.sh
@@ -1,8 +1,11 @@
+K3STOKEN = $(openssl -hex 16)
 curl -sfL https://rancher-mirror.oss-cn-beijing.aliyuncs.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn  INSTALL_K3S_EXEC=" \
                         --datastore-endpoint=mysql://db-user:db-passwd@tcp(db-ip:3306)/k3s \
-						--token=$(openssl -hex 16) \
+						--token=$K3STOKEN \
                         --node-ip $(ip addr show wg0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1) \
                         --advertise-address $(ip addr show wg0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1) \
                         --node-external-ip $(ip addr show wg0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1) \
                         --flannel-iface wg0   \
                         --node-name $HOSTNAME " sh -
+			
+echo "K3s Token is $K3STOKEN"
